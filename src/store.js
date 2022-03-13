f7333e18aa8097611f07e53a10da46e57a736dcc
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2022-03-13 22:34:09 +0200

    fix 11.21.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
